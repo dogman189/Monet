51f14090bee7736f8c773cda82631782464947a9
Aanetra Vaidya <aanetra.vaidya@gmail.com>
2026-04-16 18:07:43 -0400

    updated install file

--- install.bat
+++
@@ -1,5 +0,0 @@
-New-Item -Path ".env" -ItemType "File" -Value "CMC_API_KEY="
-python -m venv venv
-./venv/Scripts/activate
-pip install -r requirements.txt
-echo "Install Complete! "
---
+++ install.ps1
@@ -0,0 +1,5 @@
+
+python -m venv venv
+./venv/Scripts/activate
+pip install -r requirements.txt
+echo "Install Complete! Create .env file in this directory"
